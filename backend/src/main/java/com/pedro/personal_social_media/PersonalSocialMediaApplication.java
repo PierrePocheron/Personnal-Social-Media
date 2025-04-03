5803111737551e632040481e24abf239f4204944
Pierre POCHERON <pierre.pocheron@gmail.com>
2025-04-03 18:53:42 +0200

    chore: init app backend

---
+++ backend/src/main/java/com/pedro/personal_social_media/PersonalSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.personal_social_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalSocialMediaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pedro/personal_social_media/PersonalSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.personal_social_media;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
